217d8976cec1bea0298c5af90eb566f69fac56eb
Ubuntu <ubuntu@vps-232cb3f7.vps.ovh.net>
2025-08-14 16:37:58 +0000

    init pimcore skeleton

---
+++ config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
+    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
+    Pimcore\Bundle\StaticRoutesBundle\PimcoreStaticRoutesBundle::class => ['all' => true],
+    Pimcore\Bundle\UuidBundle\PimcoreUuidBundle::class => ['all' => true],
+    Pimcore\Bundle\GenericExecutionEngineBundle\PimcoreGenericExecutionEngineBundle::class => ['all' => true],
+];
